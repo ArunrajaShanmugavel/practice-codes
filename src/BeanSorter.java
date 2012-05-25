391d1fd8e509a7bb00f01aff0878c563cbdd1b8c
ArunRaja <arunraja101@gmail.com>
2012-05-25 10:49:08 +0530

    BeanSorter & DateParser added

---
+++ src/BeanSorter.java
@@ -0,0 +1,88 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * Input from file Name-Sal, Sort them , Output to a file
+ * @author arun
+ *
+ */
+
+public class BeanSorter {
+	
+	public static void main(String s[]) throws IOException
+	{
+		
+		FileInputStream fis = new FileInputStream("/Users/arun/IdeaProjects/file-ip-test.txt");
+		
+		Scanner sc = new Scanner (fis, "UTF-8");
+		
+		ArrayList<bean1> al = new ArrayList<bean1>();
+				
+		while(sc.hasNextLine())
+		{
+			String s1;
+			System.out.println(  s1 = sc.nextLine());
+			String inArr[] = s1.split("-");
+			al.add(new bean1(inArr[0],Integer.parseInt(inArr[1])));
+		}
+		
+		Collections.sort(al,new BeanComparator() );
+		
+		FileOutputStream fos = new FileOutputStream ("/Users/arun/IdeaProjects/file-op-test.txt");
+		
+		OutputStreamWriter dos = new OutputStreamWriter (fos,"UTF-8");
+		
+		StringBuilder sb = new StringBuilder(); 
+		
+		for (bean1 b1: al){
+			sb.append(b1.name+"-"+b1.sal+"\n");
+			log(sb.toString());
+		}
+		
+		dos.append(sb);
+		
+		dos.close(); sc.close();
+		
+	}
+	
+	static void  log(String s)
+	{
+		System.out.println(s);
+	}
+
+}
+
+class bean1{
+	
+	String name;
+	int sal;
+	
+	bean1(String name, int sal){
+		this.name = name;
+		this.sal = sal;
+	}
+	
+}
+
+class BeanComparator implements Comparator<bean1>
+{
+	
+	public int compare(bean1 o1, bean1 o2) {
+		
+		if(o1.sal > o2.sal)
+			return -1;
+		else if (o1.sal < o2.sal)
+			return 1;
+		else
+			return 0;
+		
+	}
+	
+}
---
+++ src/DateParser.java
@@ -0,0 +1,40 @@
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+
+public class DateParser {
+	
+	public static void main(String s[]) throws ParseException
+	{
+		DateFormat formatter ; 
+		 Date date ; 
+		 formatter = new SimpleDateFormat("dd-MM-yy");
+		 date = (Date)formatter.parse("10-5-12");  
+		 System.out.println("Today is " +date );
+		 
+		 formatter = new SimpleDateFormat("HH a");
+		 date = (Date)formatter.parse("12 AM"); 
+		 System.out.println("Today is " + date.getTime() );
+		 
+		 
+		 System.out.println(TimeUnit.MILLISECONDS.toSeconds(10000));
+	}
+	
+	static void log(String s)
+	{
+		System.out.println(s);
+	}
+	
+	public String getTimeDiff(Date dateOne, Date dateTwo) {
+        String diff = "";
+        long timeDiff = Math.abs(dateOne.getTime() - dateTwo.getTime());
+        diff = String.format("%d hour(s) %d min(s)", TimeUnit.MILLISECONDS.toHours(timeDiff),
+                TimeUnit.MILLISECONDS.toMinutes(timeDiff) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(timeDiff)));
+        return diff;
+}
+
+}
